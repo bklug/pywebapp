f9f87b8683bf12d7ad4ee5e9206fcb14aac703e7
bklug <bklug@bcit.ca>
2019-11-06 11:45:27 -0800

    Removed the latest tag

--- appspec/startcontainer.sh
+++ appspec/startcontainer.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 $(aws ecr get-login --no-include-email --region us-west-2)
-docker pull 634706590864.dkr.ecr.us-west-2.amazonaws.com/billklug/pywebapp:latest || {
+docker pull 634706590864.dkr.ecr.us-west-2.amazonaws.com/billklug/pywebapp || {
     echo "ERROR: docker pull failed. Sleeping for 10 minutes to allow investigation..."
     sleep 600
     exit 1
